e97ebc9d81cf7d83bc3bbb71896b260dc93e05e4
Jesse Rosenfield <handiworknyc.com@gmail.com>
2025-12-01 13:55:44 -0600

    static

--- astro.config.bkp.mjs
+++ astro.config.bkp.mjs
@@ -1,18 +1,26 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify/static';
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 import { fileURLToPath } from 'node:url';
 import { resolve } from 'node:path';
 
 const root = fileURLToPath(new URL('.', import.meta.url));
 
+if (process.env.NODE_ENV === "development") {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+}
+
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'static',          // ⬅ FULL STATIC MODE
-  adapter: netlify(),        // ⬅ static adapter, not server adapter
+  output: 'server',
+  adapter: netlify(),
   vite: {
+    // This tells Vite it is okay to accept traffic from your Cloudflare URL
+    server: {
+      allowedHosts: ['astro-sync.handiworknyc.com']
+    },
     resolve: {
       alias: {
         '@': resolve(root, 'src'),
