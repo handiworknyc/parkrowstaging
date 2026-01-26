69ac5061e6ccd316dd7121753cca99ce92502da2
Jesse Rosenfield <handiworknyc.com@gmail.com>
2026-01-26 12:11:00 -0600

    scroll lock

--- src/scripts/scroll-lock.ts
+++ src/scripts/scroll-lock.ts
@@ -107,22 +107,11 @@ function removeScrollPrevention(): void {
 
 function lockNativeScroll(): void {
   scrollY = window.scrollY;
-
-  document.body.style.position = "fixed";
-  document.body.style.top = `-${scrollY}px`;
-  document.body.style.width = "100%";
-  document.body.style.overflowY = "scroll";
-
   addScrollPrevention();
 }
 
 function unlockNativeScroll(): void {
   removeScrollPrevention();
-
-  document.body.style.position = "";
-  document.body.style.top = "";
-  document.body.style.width = "";
-  document.body.style.overflowY = "";
 
   window.scrollTo(0, scrollY);
 }
